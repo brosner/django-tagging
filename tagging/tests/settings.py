3135fc379b6d7b90a8d6347f45a76a4b5fe8b834
Brian Rosner <brosner@gmail.com>
2009-08-23 04:42:55 +0000

    [0.3.X] modified test settings for flexibility on testing against multiple databases
    Backport of r161 from trunk
    
    git-svn-id: https://django-tagging.googlecode.com/svn/branches/0.3.X@162 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tagging/tests/settings.py
+++ tagging/tests/settings.py
@@ -3,22 +3,22 @@ DIRNAME = os.path.dirname(__file__)
 
 DEFAULT_CHARSET = 'utf-8'
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = os.path.join(DIRNAME, 'tagging_test.db')
+test_engine = os.environ.get("TAGGING_TEST_ENGINE", "sqlite3")
 
-#DATABASE_ENGINE = 'mysql'
-#DATABASE_NAME = 'tagging_test'
-#DATABASE_USER = 'root'
-#DATABASE_PASSWORD = ''
-#DATABASE_HOST = 'localhost'
-#DATABASE_PORT = '3306'
+DATABASE_ENGINE = test_engine
+DATABASE_NAME = os.environ.get("TAGGING_DATABASE_NAME", "tagging_test")
+DATABASE_USER = os.environ.get("TAGGING_DATABASE_USER", "")
+DATABASE_PASSWORD = os.environ.get("TAGGING_DATABASE_PASSWORD", "")
+DATABASE_HOST = os.environ.get("TAGGING_DATABASE_HOST", "localhost")
 
-#DATABASE_ENGINE = 'postgresql_psycopg2'
-#DATABASE_NAME = 'tagging_test'
-#DATABASE_USER = 'postgres'
-#DATABASE_PASSWORD = ''
-#DATABASE_HOST = 'localhost'
-#DATABASE_PORT = '5432'
+if test_engine == "sqlite":
+    DATABASE_NAME = os.path.join(DIRNAME, 'tagging_test.db')
+    DATABASE_HOST = ""
+elif test_engine == "mysql":
+    DATABASE_PORT = os.environ.get("TAGGING_DATABASE_PORT", 3306)
+elif test_engine == "postgresql_psycopg2":
+    DATABASE_PORT = os.environ.get("TAGGING_DATABASE_PORT", 5432)
+
 
 INSTALLED_APPS = (
     'django.contrib.contenttypes',
